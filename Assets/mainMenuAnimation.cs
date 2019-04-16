54895fd8043eb352d0a51ea9a3663c162152105d
Christian Dunkel <christian.dunkel@uni-weimar.de>
2019-04-17 01:32:55 +0200

    added spinning circle animation to main menu

---
+++ Assets/mainMenuAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainMenuAnimation : MonoBehaviour {
+
+  private float zRotation = 5.0f;
+
+    // called once per frame
+    void Update() {
+
+      // update circle rotation on main menu screen
+      zRotation += 6;
+
+      // keep it in 360 degrees
+      zRotation %= 360;
+
+      gameObject.transform.eulerAngles = new Vector3(
+        gameObject.transform.eulerAngles.x,
+        gameObject.transform.eulerAngles.y,
+        -zRotation
+      );    
+    
+    }
+
+}

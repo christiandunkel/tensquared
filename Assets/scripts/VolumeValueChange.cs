5012ca9f41e711f88a73bc55c2465c42f6ce6731
Christian Dunkel <christian.dunkel@uni-weimar.de>
2019-04-16 22:55:23 +0200

    added slider to control music volume

---
+++ Assets/scripts/VolumeValueChange.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class VolumeValueChange : MonoBehaviour {
+
+  // reference to audio source component
+  private AudioSource audioSrc;
+  private float musicVolume = 1f;
+
+  // initialization
+  void Start() {
+    // assign audio source component
+    audioSrc = GetComponent<AudioSource>();
+  }
+
+  // called once per frame
+  void Update() {
+    // set volume of audio source equal to musicVolume given in options
+    audioSrc.volume = musicVolume;
+  }
+
+  // called by slider game object
+  public void SetVolume(float newVolume) {
+
+    Debug.Log("Change music volume to " + newVolume);
+
+    musicVolume = newVolume;
+  }
+
+}

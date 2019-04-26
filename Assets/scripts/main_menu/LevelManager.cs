31815443467a34b6520a04db8a62575d46b81595
Christian Dunkel <christiandunkel.contact@gmail.com>
2019-04-26 18:37:09 +0200

    added level manager to disable lvl buttons not yet unlocked

---
+++ Assets/scripts/main_menu/LevelManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+
+  public int LevelsUnlocked = 1;
+  public List<GameObject> LevelButton;
+
+  void Start()
+  {
+    // get children (lvl buttons) using transform property
+    foreach (Transform child in transform)
+    {
+      LevelButton.Add(child.gameObject);
+    }
+
+    int counter = 1;
+    foreach (GameObject lvl in LevelButton)
+    {
+
+      // disable buttons for levels not yet unlocked
+      if (counter > LevelsUnlocked)
+      {
+        CanvasGroup CG = lvl.GetComponent<CanvasGroup>();
+        CG.alpha = 0.2f; // opacity
+        CG.interactable = false;
+      }
+
+      counter++;
+    }
+
+  }
+
+}

f9eb15268a9053cfd755859a99f544821c2f6826
Christian Dunkel <christiandunkel.contact@gmail.com>
2019-05-06 22:41:18 +0200

    added export and import menu

---
+++ Assets/Scripts/SaveLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveLoader : MonoBehaviour
+{
+    
+  void Start()
+  {
+
+    Debug.Log(Resources.Load("test.txt"));
+
+    if (false)
+    {
+      Debug.Log("yes");
+    }
+    else
+    {
+      Debug.Log("no");
+    }
+
+  }
+
+}

f7a89acb4c6316f78b614225890e6c05c8a68676
Christian Dunkel <christiandunkel.contact@gmail.com>
2019-05-06 23:46:59 +0200

    added 'save text' generation in 'export save' menu

--- Assets/Scripts/SaveLoader.cs
+++ Assets/Scripts/SaveLoader.cs
@@ -1,14 +1,87 @@
-﻿using System.Collections;
+﻿using System;
+using System.Text;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SaveLoader : MonoBehaviour
 {
+
+  private class VolumeSettings
+  {
+    public string music = "0.0f";
+    public string sound = "0.0f";
+    public string speech = "0.0f";
+  }
+
+  private class Progress
+  {
     
+  }
+
   void Start()
   {
 
     
+
+  }
+
+
+
+
+  public InputField exportField = null;
+  public Toggle exportSettings = null;
+
+  public void LoadExportData()
+  {
+
+    if (exportField == null)
+    {
+      Debug.Log("No 'export field' set in SaveLoader.");
+      return;
+    }
+
+    if (exportSettings == null)
+    {
+      Debug.Log("No 'export settings button' set in SaveLoader.");
+      return;
+    }
+
+    string save_data = "";
+
+    if (exportSettings.isOn == true)
+    {
+
+      VolumeSettings volume = GetVolumeSettings();
+
+      save_data += "music_volume=" + volume.music + "|";
+      save_data += "sound_volume=" + volume.sound + "|";
+      save_data += "speech_volume=" + volume.speech + "|";
+
+    }
+
+    save_data += "progess";
+
+
+    // encode with base64
+    byte[] bytesToEncode = Encoding.UTF8.GetBytes(save_data);
+    save_data = Convert.ToBase64String(bytesToEncode);
+
+    exportField.text = save_data;
+
+  }
+
+  private VolumeSettings GetVolumeSettings()
+  {
+
+    VolumeSettings volume = new VolumeSettings();
+
+    volume.music = PlayerPrefs.GetFloat("music_volume").ToString();
+    volume.sound = PlayerPrefs.GetFloat("sound_volume").ToString();
+    volume.speech = PlayerPrefs.GetFloat("speech_volume").ToString();
+
+    return volume;
 
   }
 

17a2697c3d48b19677abd6e9ea336f4742bd4863
Christian Dunkel <christiandunkel.contact@gmail.com>
2019-05-08 06:41:53 +0200

    added 'jumping letters' text animation

---
+++ Assets/Scripts/JumpingTextAnimator.cs
@@ -0,0 +1,90 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * @basic script by https://www.youtube.com/watch?v=JubT4ldOwZQ 
+ */
+public class JumpingTextAnimator : MonoBehaviour
+{
+  public float waitBetween;
+  public float waitEnd;
+
+  public int jumpItemCount;
+  int currentItem;
+
+  public bool reverse;
+
+  public bool alwaysAnimateNext = true;
+  public bool alwaysReverseOnEnd;
+  public bool alwaysUpdateList;
+
+  List<Animator> animators;
+
+  void Start()
+  {
+    UpdateAnimatorList();
+
+    StartCoroutine(Play());
+  }
+
+  public IEnumerator Play()
+  {
+    while (true)
+    {
+      if (!alwaysAnimateNext)
+      {
+        yield return new WaitForEndOfFrame();
+
+        continue;
+      }
+
+      if (alwaysUpdateList)
+      {
+        UpdateAnimatorList();
+      }
+
+      IEnumerable<Animator> tempAnimators = animators;
+
+      if (reverse)
+      {
+        tempAnimators.Reverse();
+      }
+
+      foreach (Animator animator in tempAnimators)
+      {
+        if (currentItem == System.Int32.MaxValue)
+        {
+          currentItem = 0;
+        }
+
+        currentItem++;
+
+        if (jumpItemCount + 1 != 0 && currentItem % (jumpItemCount + 1) != 0)
+        {
+          continue;
+        }
+
+        animator.SetTrigger("DoAnimation");
+
+        if (!Mathf.Approximately(waitBetween, 0))
+        {
+          yield return new WaitForSeconds(waitBetween);
+        }
+      }
+
+      yield return new WaitForSeconds(waitEnd);
+
+      if (alwaysReverseOnEnd)
+      {
+        reverse = !reverse;
+      }
+    }
+  }
+
+  public void UpdateAnimatorList()
+  {
+    animators = GetComponentsInChildren<Animator>().ToList();
+  }
+}

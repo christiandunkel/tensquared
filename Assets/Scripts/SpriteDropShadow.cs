82e5cfb972b1b0c5b6b0aa928cf6e8d5df908a7a
Christian Dunkel <christiandunkel.contact@gmail.com>
2019-05-14 23:37:07 +0200

    added custom drop shadows, adjusted cloud textures, compressed textures, fixed about menu lighting

---
+++ Assets/Scripts/SpriteDropShadow.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteDropShadow : MonoBehaviour
+{
+
+  public Vector3 shadowOffset = new Vector3(0.5f, 0.5f, 0f);
+  public Material material;
+
+  private GameObject shadow;
+
+  void Start()  {
+
+    shadow = new GameObject("Shadow");
+    shadow.transform.parent = transform;
+
+    shadow.transform.localPosition = shadowOffset;
+    shadow.transform.localRotation = Quaternion.identity;
+
+    SpriteRenderer sr_original = GetComponent<SpriteRenderer>();
+    SpriteRenderer sr_shadow = shadow.AddComponent<SpriteRenderer>();
+
+    sr_shadow.sprite = sr_original.sprite;
+    sr_shadow.material = material;
+
+    sr_shadow.sortingLayerName = sr_original.sortingLayerName;
+    sr_shadow.sortingOrder = sr_original.sortingOrder - 1;
+
+    sr_shadow.transform.localScale = new Vector3(1f, 1f, 1f);
+
+  }
+
+  private void LateUpdate() {
+    shadow.transform.localPosition = shadowOffset;
+  }
+
+}

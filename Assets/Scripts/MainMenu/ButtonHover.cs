b3564224b650793adfa5600207708f4c4fc17247
Christian Dunkel <christiandunkel.contact@gmail.com>
2019-05-15 18:10:02 +0200

    added hover 'pop out' effect to play button, improved hover colors and design of buttons in main menu

---
+++ Assets/Scripts/MainMenu/ButtonHover.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHover : MonoBehaviour
+{
+
+  Animator animator;
+
+  void Start()
+  {
+    animator = gameObject.GetComponent<Animator>();
+  }
+
+  private bool isHovering = false;
+
+  public void Update()
+  {
+    animator.SetBool("Hovering", isHovering);
+  }
+
+  public void hoverEnter()
+  {
+    isHovering = true;
+  }
+
+  public void hoverExit()
+  {
+    isHovering = false;
+  }
+
+}

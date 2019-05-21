8e10c507e3e688b99e5a384bf87198f64c50fde6
Christian Dunkel <christiandunkel.contact@gmail.com>
2019-05-21 13:33:47 +0200

    added tooltip system and first tooltips

---
+++ Assets/Scripts/TooltipManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TooltipManager : MonoBehaviour {
+
+
+  public static TooltipManager Instance;
+
+  private static GameObject tooltipManager;
+  private static List<GameObject> tooltipList;
+
+  void Awake() {
+
+    Instance = this;
+
+    tooltipManager = gameObject;
+    tooltipList = new List<GameObject>();
+
+    // add all children (tooltips) to list
+    foreach (Transform child in tooltipManager.transform) {
+      tooltipList.Add(child.gameObject);
+      child.gameObject.SetActive(true);
+    }
+
+  }
+
+  public static void showTooltip(string name) {
+
+    GameObject obj = null;
+
+    foreach (GameObject o in tooltipList) {
+      if (o.name == name) {
+        obj = o;
+        o.GetComponent<Animator>().SetBool("Visible", true);
+      }
+      else {
+        o.GetComponent<Animator>().SetBool("Visible", false);
+      }
+    }
+
+    if (obj == null) {
+      Debug.Log("TooltipManager: Could not find tooltip with this name.");
+      return;
+    }
+
+  }
+
+  public static void hideTooltips()  {
+
+    foreach (GameObject o in tooltipList) {
+      o.GetComponent<Animator>().SetBool("Visible", false);
+    }
+
+  }
+
+}

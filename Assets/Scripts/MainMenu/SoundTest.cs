415ab32a67eb5a830bdff6d37c8fb79885e620b2
Christian Dunkel <christiandunkel.contact@gmail.com>
2019-06-04 22:56:40 +0200

    added sound test

---
+++ Assets/Scripts/MainMenu/SoundTest.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTest : MonoBehaviour {
+
+  public GameObject soundTestMessage;
+
+  public AudioClip musicClip, speechClip;
+  public AudioClip[] soundClips;
+
+  public AudioSource mainMenuMusicPlayer, musicPlayer, speechPlayer, soundPlayer;
+
+  private bool soundTestIsPlaying = false;
+  public void playSoundTest() {
+
+    if (!soundTestIsPlaying) {
+      soundTestIsPlaying = true;
+      StartCoroutine(soundTestCoroutine());
+    }
+    
+  }
+
+  public void stopSoundTest()  {
+
+    if (soundTestIsPlaying) {
+      StopCoroutine(soundTestCoroutine());
+      soundTestIsPlaying = false;
+      soundTestMessage.SetActive(false);
+      gameObject.GetComponent<CanvasGroup>().alpha = 1f;
+      gameObject.GetComponent<CanvasGroup>().interactable = true;
+      mainMenuMusicPlayer.UnPause();
+    }
+  }
+
+  IEnumerator soundTestCoroutine() {
+
+    Debug.Log("SoundTest: Started sound test.");
+
+    mainMenuMusicPlayer.Pause();
+    gameObject.GetComponent<CanvasGroup>().alpha = 0f;
+    gameObject.GetComponent<CanvasGroup>().interactable = false;
+    soundTestMessage.SetActive(true);
+
+    yield return new WaitForSeconds(.2f);
+
+    musicPlayer.PlayOneShot(musicClip);
+
+    yield return new WaitForSeconds(.2f);
+
+    speechPlayer.PlayOneShot(speechClip);
+
+    yield return new WaitForSeconds(speechClip.length + .5f);
+
+    foreach (AudioClip c in soundClips) {
+      soundPlayer.PlayOneShot(c);
+      yield return new WaitForSeconds(c.length + .3f);
+    }
+
+    yield return new WaitForSeconds(.2f);
+
+    musicPlayer.Stop();
+
+    yield return new WaitForSeconds(.2f);
+
+    soundTestMessage.SetActive(false);
+    gameObject.GetComponent<CanvasGroup>().alpha = 1f;
+    gameObject.GetComponent<CanvasGroup>().interactable = true;
+    mainMenuMusicPlayer.UnPause();
+    soundTestIsPlaying = false;
+
+    Debug.Log("SoundTest: Sound test ended.");
+
+    StopCoroutine(soundTestCoroutine());
+
+  }
+
+}

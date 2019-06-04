a16f5e2755c943744d9e37d6f5b58960fc0b2473
Christian Dunkel <christiandunkel.contact@gmail.com>
2019-06-05 01:07:33 +0200

    added teleporting block

---
+++ Assets/Scripts/TeleportingBlock.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportingBlock : MonoBehaviour {
+
+  public Animator squareTexture1, squareTexture2;
+
+  public float stayTime = 1.5f,
+               timeBothAreVisible = .8f;
+
+  private void Awake() {
+    startCycle();
+  }
+
+  private void startCycle() {
+    StopCoroutine(cycle());
+    StartCoroutine(cycle());
+  }
+
+  IEnumerator cycle() {
+
+    squareTexture1.SetBool("Visible", true);
+
+    yield return new WaitForSeconds(timeBothAreVisible);
+
+    squareTexture2.SetBool("Visible", false);
+
+    yield return new WaitForSeconds(stayTime);
+
+    squareTexture2.SetBool("Visible", true);
+
+    yield return new WaitForSeconds(timeBothAreVisible);
+
+    squareTexture1.SetBool("Visible", false);
+
+    yield return new WaitForSeconds(stayTime);
+
+    startCycle();
+
+  }
+
+}

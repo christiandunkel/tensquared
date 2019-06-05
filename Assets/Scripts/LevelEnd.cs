559b26d8732948e53417d6d9cb4e01a8e20c3630
Christian Dunkel <christiandunkel.contact@gmail.com>
2019-06-05 20:34:46 +0200

    added 'level completed' animation to 'level end' script

--- Assets/Scripts/LevelEnd.cs
+++ Assets/Scripts/LevelEnd.cs
@@ -8,11 +8,31 @@ public class LevelEnd : MonoBehaviour {
 
   public int levelID = 1;
 
-  private CanvasGroup CG;
+  private CanvasGroup CG, endMenuContainer, levelCompleteCG, nextLevelCG;
 
   void Awake() {
+
     Instance = this;
     CG = gameObject.GetComponent<CanvasGroup>();
+
+    // get animators of child elements
+    foreach (Transform child in transform) {
+
+      if (child.gameObject.name != "LevelEndMenu") continue;
+
+      endMenuContainer = child.gameObject.GetComponent<CanvasGroup>();
+
+      foreach (Transform child2 in child.gameObject.transform)
+      {
+        switch (child2.gameObject.name)
+        {
+          case "LevelCompleteAnimation": levelCompleteCG = child2.gameObject.GetComponent<CanvasGroup>(); break;
+          case "NextLevelAnimation": nextLevelCG = child2.gameObject.GetComponent<CanvasGroup>(); break;
+          default: break;
+        }
+      }
+
+    }
   }
 
   public void endLevel() {
@@ -22,7 +42,7 @@ public class LevelEnd : MonoBehaviour {
     LevelSettings.Instance.SetSetting("canMove", false);
     LevelSettings.Instance.SetSetting("canMorph", false);
 
-    PauseMenu.Instance.allowOpeningPauseMenu = false;
+    PauseMenu.Instance.gameObject.SetActive(false);
 
     // activate next level
     if (!PlayerPrefs.HasKey("lvls_unlocked")) PlayerPrefs.SetInt("lvls_unlocked", levelID + 1);
@@ -31,6 +51,36 @@ public class LevelEnd : MonoBehaviour {
     CG.alpha = 1f;
     CG.interactable = true;
 
+    StartCoroutine(endSceenAnimation());
+
+  }
+
+  private IEnumerator endSceenAnimation() {
+
+    for (int i = 0; i < 50; i++) {
+      endMenuContainer.alpha = ((float)i) * 2f / 100f;
+      yield return new WaitForSeconds(0.01f);
+    }
+    endMenuContainer.alpha = 1f;
+    endMenuContainer.interactable = true;
+
+    yield return new WaitForSeconds(3f);
+
+    for (int i = 0; i < 50; i++) {
+      levelCompleteCG.alpha = 1 - (((float)i) * 2f / 100f);
+      yield return new WaitForSeconds(0.006f);
+    }
+    levelCompleteCG.alpha = 0f;
+
+    yield return new WaitForSeconds(.2f);
+
+    for (int i = 0; i < 50; i++) {
+      nextLevelCG.alpha = ((float)i) * 2f / 100f;
+      yield return new WaitForSeconds(0.006f);
+    }
+    nextLevelCG.alpha = 1f;
+    nextLevelCG.interactable = true;
+    StopCoroutine(endSceenAnimation());
   }
 
   public void goToNextLevel() {

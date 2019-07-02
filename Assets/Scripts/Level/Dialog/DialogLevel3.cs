783c4b4f6a5551e79dfe537a426d30bf6fa7a8e3
Christian Dunkel <christiandunkel.contact@gmail.com>
2019-07-02 17:28:39 +0200

    added dialogue choices for level 3

--- Assets/Scripts/Level/Dialog/DialogLevel3.cs
+++ Assets/Scripts/Level/Dialog/DialogLevel3.cs
@@ -40,6 +40,106 @@ public class DialogLevel3 : MonoBehaviour {
         dialog.icon = "happy";
         break;
 
+      case "lvl3_EVIL_like_the_melody":
+        dialog.setText(
+          "The melody in my head won't stop playing...",
+          "I like it."
+        );
+        dialog.setAudioClip("lvl3_EVIL_like_the_melody");
+        dialog.icon = "happy";
+        dialog.isEvil = true;
+        break;
+
+      case "lvl3_neck_nose_ear_specialist":
+        dialog.setText(
+          "Can you recommend a good ear, nose and throat specialist? " + 
+          "I think I have a problem with my ears."
+        );
+        dialog.setAudioClip("lvl3_neck_nose_ear_specialist");
+        dialog.icon = "neutral";
+        break;
+
+      case "lvl3_smoking_legs":
+        dialog.setText(
+          "Hey, little friend. Those new legs you brought me... well, they work, but... they seem to smoke, did you notice that?"
+        );
+        dialog.setAudioClip("lvl3_smoking_legs");
+        dialog.icon = "sad";
+        break;
+
+      case "lvl3_messed_up_place":
+        dialog.setText(
+          "Anyway, what happened to this place? Why does all this toxic water leak out? Did you do that, little friend?"
+        );
+        dialog.setAudioClip("lvl3_messed_up_place");
+        dialog.icon = "angry";
+        break;
+
+      case "lvl3_EVIL_fruit_juice_shooters":
+        dialog.setText(
+          "Don't worry. Those laser cannons... I mean, fruit juice shooters are totally harmless."
+        );
+        dialog.setAudioClip("lvl3_EVIL_fruit_juice_shooters");
+        dialog.icon = "happy";
+        dialog.isEvil = true;
+        break;
+
+      case "lvl3_surprised_about_laser_cannons":
+        dialog.setText(
+          "Wow, they actually malfunctioned and didn't shoot. Didn't I tell you, they were safe? Just as I expected."
+        );
+        dialog.setAudioClip("lvl3_surprised_about_laser_cannons");
+        dialog.icon = "happy";
+        break;
+
+      case "lvl3_EVIL_set_on_fire":
+        dialog.setText(
+          "It smells like barbecue. Did you set something on fire?"
+        );
+        dialog.setAudioClip("lvl3_EVIL_set_on_fire");
+        dialog.icon = "neutral";
+        dialog.isEvil = true;
+        break;
+
+      case "lvl3_toxic_gases":
+        dialog.setText(
+          "There's chlorine in the air. And do I smell a little arsine? " + 
+          "Either way, it's no problem to you. You're a circle, " + 
+          "and I've never seen a circle die of toxic gases."
+        );
+        dialog.setAudioClip("lvl3_toxic_gases");
+        dialog.icon = "neutral";
+        break;
+
+      case "lvl3_melody_is_gone":
+        dialog.setText(
+          "I think I feel a little better now.",
+          "The strange melody is gone."
+        );
+        dialog.setAudioClip("lvl3_melody_is_gone");
+        dialog.icon = "happy";
+        break;
+
+      case "lvl3_EVIL_unfortunate":
+        dialog.setText(
+          "Well, that is truly unfortunate.",
+          "And it hurt a little."
+        );
+        dialog.setAudioClip("lvl3_EVIL_unfortunate");
+        dialog.icon = "neutral";
+        dialog.isEvil = true;
+        break;
+
+      case "lvl3_EVIL_hand_over_your_body":
+        dialog.setText(
+          "Anyway, that's over. Do you mind handing over your scientifically impossible body to me? " + 
+          "There are a few things I would like to experiment with."
+        );
+        dialog.setAudioClip("lvl3_EVIL_hand_over_your_body");
+        dialog.icon = "laughing";
+        dialog.isEvil = true;
+        break;
+
     }
 
     return dialog;
--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 [![tensquared Poster](/Posters/poster_500x707px_compressed.png)](#)
 
-The 2D puzzle platformer **tensquared** was made in Unity for the *8-bit Bauhaus* study project in the summer semester of 2019 at the *Bauhaus University Weimar*. The project was started to create a number of Bauhaus-themed games in order to celebrate 100 years of Bauhaus. See the [documentation](/Documentation/Documentation.md) for more information.
+The 2D puzzle platformer **tensquared** was made in Unity for the *8-bit Bauhaus* study project in the summer semester of 2019 at the *Bauhaus University Weimar*. The project was started to create a number of Bauhaus-themed games in order to celebrate 100 years of Bauhaus.
+
+See the [documentation](/Documentation/Documentation.md) for more information.
 
 The game is being developed by **Christian** (Programming), **Nyiko** (Animation, Graphics) and **Raphael** (Audio, Graphics). The game is currently in *live development*. You can download the project up to the newest commit, or select one of the releases. You can play it in the Unity editor or build it. The game in its newest version requires **Unity version 2019.1.4f1**. A newer version than that should work fine as well, probably.
 
-[![Bauhaus Logo](/Documentation/bauhaus_logo_large.png)](#)
+[![Bauhaus Logo](/Documentation/Images/bauhaus_logo_large.png)](#)
 
 What is *Bauhaus*? The Bauhaus was originally a German art school founded in Weimar by Walter Gropius, being operational from 1919 to 1933. Later, new schools in two German cities, Dessau and Berlin, were founded. Today, upholding modernism, rationality and functionality, the Bauhaus lives on as a way of design in art, architecture and industrial design.
 
